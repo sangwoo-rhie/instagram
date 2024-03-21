c29ba957c713e21835d6f48f54dae6bd2c61412b
Sangwoo rhie <powercom92@naver.com>
2024-03-21 13:24:56 +0900

    storing comments in firestore

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -149,7 +149,7 @@ class _PostCardState extends State<PostCard> {
                   onPressed: () => Navigator.of(context).push(
                     MaterialPageRoute(
                       builder: (context) => CommentsScreen(
-                        snap: widget.snap['postId'].toString();
+                        snap: widget.snap['postId'].toString()
                       ),
                       ),),
                   icon: const Icon(
