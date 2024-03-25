014ebc8be8081e87e269770655abc422d42a08bc
sangwoorhie <powercom92@naver.com>
2024-03-25 11:12:47 +0900

    Creating Reusable Profile Screen UI

--- lib/screen/profile_screen.dart
+++ lib/screen/profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/utils/colors.dart';
+import 'package:instagram_flutter/widgets/follow_button.dart';
 
 // 내 프로필 스크린
 class ProfileScreen extends StatefulWidget {
@@ -21,35 +22,63 @@ class _ProfileScreenState extends State<ProfileScreen> {
       body: ListView(
         children: [
           Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              children: [
-                CircleAvatar(
-                  backgroundColor: Colors.grey,
-                  backgroundImage: NetworkImage(""),
-                  radius: 40,
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      buildStatColumn(20, 'posts'),
-                      buildStatColumn(150, 'followers'),
-                      buildStatColumn(10, 'following'),
-                    ],
+              padding: const EdgeInsets.all(16),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    backgroundColor: Colors.grey,
+                    backgroundImage: NetworkImage(""),
+                    radius: 40,
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Column(
+                      children: [
+                        // 게시글, 팔로워수, 팔로잉수 Row
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            buildStatColumn(20, 'posts'), // 게시글 수
+                            buildStatColumn(150, 'followers'), // 팔로워 수
+                            buildStatColumn(10, 'following'), // 팔로잉 수
+                          ],
+                        ),
+                        // 프로필 수정 Row
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            FollowButton(
+                              text: 'Edit Profile',
+                              backgroundColor: mobileBackgroundColor,
+                              textColor: primaryColor,
+                              borderColor: Colors.grey,
+                              function: () {},
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: const EdgeInsets.only(top: 15),
+                child: Text(
+                  'username',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    followButton(),
-                  ],
-                )
-              ],
-            ),
-          )
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: const EdgeInsets.only(top: 1),
+                child: Text(
+                  'some description',
+                ),
+              ))
         ],
       ),
     );
