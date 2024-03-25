da71d806072d8851573cad4c9b23e87ba65332ed
sangwoorhie <powercom92@naver.com>
2024-03-25 12:21:44 +0900

    Displaying Profile Data

--- lib/screen/profile_screen.dart
+++ lib/screen/profile_screen.dart
@@ -1,87 +1,205 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/utils/colors.dart';
+import 'package:instagram_flutter/utils/utils.dart';
 import 'package:instagram_flutter/widgets/follow_button.dart';
 
 // 내 프로필 스크린
 class ProfileScreen extends StatefulWidget {
-  const ProfileScreen({Key? key}) : super(key: key);
+  final String uid;
+  const ProfileScreen({Key? key, required this.uid}) : super(key: key);
 
   @override
   _ProfileScreenState createState() => _ProfileScreenState();
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  var userData = {};
+  int postLength = 0; // 게시글수 (디폴트값 0)
+  int followers = 0; // 팔로워수 (디폴트값 0)
+  int following = 0; // 팔로잉수 (디폴트값 0)
+  bool isFollowing = false; // 팔로잉여부 (디폴트값 false)
+  bool isLoading = false; // 로딩여부 (디폴트값 false)
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  getData() async {
+    setState(() {
+      isLoading = true;
+    });
+    try {
+      var userSnap = await FirebaseFirestore.instance
+          .collection('user')
+          .doc(widget.uid)
+          .get();
+
+      // get Post length
+      var postSnap = await FirebaseFirestore.instance
+          .collection('posts')
+          .where('uid', isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+          .get();
+
+      userData = userSnap.data()!;
+      postLength = postSnap.docs.length; // 게시글 수
+      followers = userSnap.data()!['followers'].length; // 팔로워 수
+      following = userSnap.data()!['following'].length; // 팔로잉 수
+      isFollowing = userSnap
+          .data()!['followers']
+          .contains(FirebaseAuth.instance.currentUser!.uid);
+
+      setState(() {});
+    } catch (error) {
+      showSnackBar(
+        context,
+        error.toString(),
+      );
+    }
+    setState(() {
+      isLoading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: mobileBackgroundColor,
-        title: Text('username'),
-        centerTitle: false,
-      ),
-      body: ListView(
-        children: [
-          Padding(
-              padding: const EdgeInsets.all(16),
-              child: Row(
-                children: [
-                  CircleAvatar(
-                    backgroundColor: Colors.grey,
-                    backgroundImage: NetworkImage(""),
-                    radius: 40,
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: Column(
-                      children: [
-                        // 게시글, 팔로워수, 팔로잉수 Row
-                        Row(
-                          mainAxisSize: MainAxisSize.min,
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    return isLoading
+        ? const Center(
+            child: CircularProgressIndicator(),
+          )
+        : Scaffold(
+            appBar: AppBar(
+              backgroundColor: mobileBackgroundColor,
+              title: Text(userData['username']),
+              centerTitle: false,
+            ),
+            body: ListView(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Row(
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: Colors.grey,
+                        backgroundImage: NetworkImage(userData['photoUrl']),
+                        radius: 40,
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Column(
                           children: [
-                            buildStatColumn(20, 'posts'), // 게시글 수
-                            buildStatColumn(150, 'followers'), // 팔로워 수
-                            buildStatColumn(10, 'following'), // 팔로잉 수
+                            // 게시글, 팔로워수, 팔로잉수 Row
+                            Row(
+                              mainAxisSize: MainAxisSize.min,
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                buildStatColumn(postLength, 'posts'), // 게시글 수
+                                buildStatColumn(
+                                    followers, 'followers'), // 팔로워 수
+                                buildStatColumn(
+                                    following, 'following'), // 팔로잉 수
+                              ],
+                            ),
+                            // 프로필 수정 Row
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                FirebaseAuth.instance.currentUser!.uid ==
+                                        widget.uid
+                                    ?
+                                    // 내정보 수정 버튼
+                                    FollowButton(
+                                        text: 'Edit Profile',
+                                        backgroundColor: mobileBackgroundColor,
+                                        textColor: primaryColor,
+                                        borderColor: Colors.grey,
+                                        function: () {},
+                                      )
+                                    :
+                                    // 언팔로우 버튼
+                                    isFollowing
+                                        ? FollowButton(
+                                            text: 'Unfollow',
+                                            backgroundColor: Colors.white,
+                                            textColor: Colors.black,
+                                            borderColor: Colors.grey,
+                                            function: () {},
+                                          )
+                                        :
+                                        // 팔로우 버튼
+                                        FollowButton(
+                                            text: 'Follow',
+                                            backgroundColor: Colors.blue,
+                                            textColor: Colors.white,
+                                            borderColor: Colors.blue,
+                                            function: () {},
+                                          )
+                              ],
+                            )
                           ],
                         ),
-                        // 프로필 수정 Row
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            FollowButton(
-                              text: 'Edit Profile',
-                              backgroundColor: mobileBackgroundColor,
-                              textColor: primaryColor,
-                              borderColor: Colors.grey,
-                              function: () {},
-                            ),
-                          ],
-                        )
-                      ],
+                      ),
+                    ],
+                  ),
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    padding: const EdgeInsets.only(top: 15),
+                    child: Text(
+                      userData['username'],
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
-                ],
-              ),
-              Container(
-                alignment: Alignment.centerLeft,
-                padding: const EdgeInsets.only(top: 15),
-                child: Text(
-                  'username',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    padding: const EdgeInsets.only(top: 1),
+                    child: Text(
+                      userData['bio'],
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                alignment: Alignment.centerLeft,
-                padding: const EdgeInsets.only(top: 1),
-                child: Text(
-                  'some description',
-                ),
-              ))
-        ],
-      ),
-    );
+                const Divider(),
+                FutureBuilder(
+                    future: FirebaseFirestore.instance
+                        .collection('posts')
+                        .where('uid', isEqualTo: widget.uid)
+                        .get(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                      return GridView.builder(
+                        shrinkWrap: true,
+                        itemCount: (snapshot.data! as dynamic).docs.length,
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3,
+                          crossAxisSpacing: 5,
+                          mainAxisSpacing: 1.5,
+                          childAspectRatio: 1,
+                        ),
+                        itemBuilder: (context, index) {
+                          DocumentSnapshot snap =
+                              (snapshot.data! as dynamic).docs[index];
+
+                          return Container(
+                            child: Image(
+                              image: NetworkImage(snap['postUrl']),
+                              fit: BoxFit.cover,
+                            ),
+                          );
+                        },
+                      );
+                    })
+              ],
+            ),
+          );
   }
 
   Column buildStatColumn(int num, String label) {
--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/screen/add_post_screen.dart';
 import 'package:instagram_flutter/screen/feed_screen.dart';
@@ -6,10 +7,12 @@ import 'package:instagram_flutter/screen/search.screen.dart';
 
 const webScreenSize = 600;
 
-const homeScreenItems = [
-  FeedScreen(), // Text('feed'),
-  SearchScreen(), // Text('search'),
-  AddPostScreen(), // Text('add post'),
-  // Text('notification'),
-  ProfileScreen() // Text('profile'),
+List<Widget> homeScreenItems = [
+  const FeedScreen(), // Text('feed'),
+  const SearchScreen(), // Text('search'),
+  const AddPostScreen(), // Text('add post'),
+  const Text('notification'), // Text('notification'),
+  ProfileScreen(
+    uid: FirebaseAuth.instance.currentUser!.uid,
+  ) // Text('profile'),
 ];
